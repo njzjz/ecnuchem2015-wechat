825bbb6b57e8fc2a1b2418cfa2a1a1771c4e5280
Jinzhe Zeng <njzjz@qq.com>
2018-08-16 20:25:47 +0800

    update treeview

---
+++ js/leancloud.js
@@ -0,0 +1,7 @@
+var APP_ID = 'EqNiLWx6KJ9O7XgvTcUNHFbo-gzGzoHsz';
+var APP_KEY = 'ppaibnjf30sLOgrC1iMTmX21';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
--- js/treeview.js
+++ js/treeview.js
@@ -1,8 +1,28 @@
-$(function(){
-	$("#form").submit(function(e) {
-		ajax($("#url").val(),"#main",1);
-		$("#more").hide();
-		return false;
+function fetch(time){
+	var query = new AV.Query('TreeObject');
+	query.descending('createdAt')
+	query.limit(15)
+	query.lessThan('createdAt', time)
+	query.find().then(function (comments) {
+		var html=''
+		for (var i = 0; i < comments.length; i++) {
+			var comment = comments[i];
+			var content = comment.get('content');
+			var createdAt = comment.createdAt
+			html+="<section class='treetable tree'>"+content+"<br/>--"+createdAt+"</section>"
+		}
+		$("#loading").hide()
+		$("#main").append(html)
+		$("#more").show()
+		$("#more").click(function(event) {
+			fetch(createdAt)
+		});
+	}, function (error) {
+		$("#loading").html("加载错误！")
 	});
-	ajax($("#url").val(),"#main",$("#mode").val());
-});
+}
+
+$(function(){
+	var time=new Date()
+	fetch(time)
+})
